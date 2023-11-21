2a832908f538da52ca220ec3a57f5eae38ae0a9d
AyanAgrawal25 <ayan.agrawal@students.iiit.ac.in>
2023-11-21 23:36:53 +0530

    structure + mui

---
+++ frontend/src/models/auth.model.ts
@@ -0,0 +1,20 @@
+export interface AuthState {
+  isAuthenticated: boolean;
+  accessToken: string | null;
+  refreshToken: string | null;
+  userType: string | null;
+  login: (accessToken: string, refreshToken: string) => void;
+  logout: () => void;
+}
+export interface signUp {
+  username: string;
+  password: string;
+  confirm_password: string;
+  first_name: string;
+  last_name: string;
+}
+
+export interface logIn {
+  email: string;
+  password: string;
+}
