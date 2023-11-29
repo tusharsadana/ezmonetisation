308645c8ae86f228fb103784881d9f15e1fafab5
Tushar Sadana <sadana@Tushars-MacBook-Pro.local>
2023-11-29 15:00:21 +0530

    Sign Up Page Completed

--- frontend/ez-monetization/src/pages/login/login.tsx
+++ frontend/ez-monetization/src/pages/login/login.tsx
@@ -11,11 +11,13 @@ import {
     Paper,
     TextField,
     Typography,
+    useMediaQuery,
 } from "@mui/material";
 import { signIn } from "../../services/auth.service";
 import { AuthContext } from "../../contexts/auth.context";
 import { ISignIn } from "../../models/auth.model";
 import { toast } from "react-toastify";
+import theme from "../../theme/ThemeProvider";
 
 export default function Login(): JSX.Element {
     const [inputState, setInputState] = useState<ISignIn>({
@@ -45,137 +47,143 @@ export default function Login(): JSX.Element {
                 toast.error("Login failed");
             });
     };
+    // I want to get if the user is on mobile or not using theme breakpoints
+    const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
+    const loginBox = <Box
+        sx={{
+            marginTop: 4,
+            marginBottom: 4,
+        }}
+    >
+        <Grid container>
+            <CssBaseline />
+            <Grid
+                item
+                xs={false}
+                sm={4}
+                md={6}
+                sx={{
+                    backgroundImage: "url(https://source.unsplash.com/random)",
+                    backgroundRepeat: "no-repeat",
+                    backgroundColor: (t) =>
+                        t.palette.mode === "light"
+                            ? t.palette.grey[50]
+                            : t.palette.grey[900],
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                    borderRadius: "16px 0 0 16px",
+                }}
+            />
+            <Grid
+                item
+                xs={12}
+                sm={8}
+                md={6}
+                p={isMobile ? -2 : 4}
+                component={Paper}
+                elevation={0}
+                square
+                sx={{
+                    borderTopRightRadius: '16px', // Set the top-right border-radius
+                    borderBottomRightRadius: '16px',
+                    borderBottomLeftRadius: isMobile ? '16px' : '0px',
+                    borderTopLeftRadius: isMobile ? '16px' : '0px', // Set the bottom-right border-radius
+                }}
+            >
+                <Box
+                    sx={{
+                        my: 8,
+                        mx: 4,
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                    }}
+                >
+                    <Typography component="h1" variant="h3" >
+                        Sign in
+                    </Typography>
+                    <Typography component={"p"} fontWeight={"light"}>
+                        Fill in the fields below to login to your account
+                    </Typography>
+                    <Box
+                        component="form"
+                        noValidate
+                        onSubmit={(e) => { e.preventDefault(); }}
+                        sx={{ mt: 1 }}
+
+                    >
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            autoFocus
+                            onChange={handleChange}
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                            onChange={handleChange}
+                        />
+                        <FormControlLabel
+                            control={<Checkbox value="remember" color="primary" />}
+                            label="Remember me"
+
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                            onClick={handleLogin}
+                        >
+                            Sign In
+                        </Button>
+                        <Grid container>
+                            <Grid item xs={12} md={6} mt={2}>
+                                <Link to="/signup" style={{ textDecoration: 'none' }}>
+                                    <Button variant="outlined" color="secondary" sx={{
+                                        boxShadow: 'none', // Disable the default box-shadow
+                                        fontSize: '10px', // Larger font size
+                                        textTransform: 'none', // Prevent uppercase transform
+                                    }}>
+                                        Forgot Password?
+                                    </Button>
+                                </Link>
+                            </Grid>
+                            <Grid item xs={12} md={6} mt={2}>
+                                <Link to="/signup" style={{ textDecoration: 'none' }}>
+                                    <Button variant="outlined" color="secondary" sx={{
+                                        boxShadow: 'none', // Disable the default box-shadow
+                                        fontSize: '10px', // Larger font size
+                                        textTransform: 'none', // Prevent uppercase transform
+                                    }}>
+                                        Don't have an account?
+                                    </Button>
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </Box>
+            </Grid>
+        </Grid>
+    </Box>
 
     return (
         <>
-            <Container component="main" maxWidth="lg">
-                <Box
-                    sx={{
-                        marginTop: 8,
-                    }}
-                >
-                    <Grid container>
-                        <CssBaseline />
-                        <Grid
-                            item
-                            xs={false}
-                            sm={4}
-                            md={6}
-                            sx={{
-                                backgroundImage: "url(https://source.unsplash.com/random)",
-                                backgroundRepeat: "no-repeat",
-                                backgroundColor: (t) =>
-                                    t.palette.mode === "light"
-                                        ? t.palette.grey[50]
-                                        : t.palette.grey[900],
-                                backgroundSize: "cover",
-                                backgroundPosition: "center",
-                                borderRadius: "16px 0 0 16px",
-                            }}
-                        />
-                        <Grid
-                            item
-                            xs={12}
-                            sm={8}
-                            md={6}
-                            p={4}
-                            component={Paper}
-                            elevation={0}
-                            square
-                            sx={{
-                                borderTopRightRadius: '16px', // Set the top-right border-radius
-                                borderBottomRightRadius: '16px', // Set the bottom-right border-radius
-                            }}
-                        >
-                            <Box
-                                sx={{
-                                    my: 8,
-                                    mx: 4,
-                                    display: "flex",
-                                    flexDirection: "column",
-                                    alignItems: "center",
-                                }}
-                            >
-                                <Typography component="h1" variant="h3" >
-                                    Sign in
-                                </Typography>
-                                <Typography component={"p"} fontWeight={"light"}>
-                                    Fill in the fields below to login to your account
-                                </Typography>
-                                <Box
-                                    component="form"
-                                    noValidate
-                                    onSubmit={(e) => { e.preventDefault(); }}
-                                    sx={{ mt: 1 }}
-
-                                >
-                                    <TextField
-                                        margin="normal"
-                                        required
-                                        fullWidth
-                                        id="email"
-                                        label="Email Address"
-                                        name="email"
-                                        autoComplete="email"
-                                        autoFocus
-                                        onChange={handleChange}
-                                    />
-                                    <TextField
-                                        margin="normal"
-                                        required
-                                        fullWidth
-                                        name="password"
-                                        label="Password"
-                                        type="password"
-                                        id="password"
-                                        autoComplete="current-password"
-                                        onChange={handleChange}
-                                    />
-                                    <FormControlLabel
-                                        control={<Checkbox value="remember" color="primary" />}
-                                        label="Remember me"
-
-                                    />
-                                    <Button
-                                        type="submit"
-                                        fullWidth
-                                        variant="contained"
-                                        sx={{ mt: 3, mb: 2 }}
-                                        onClick={handleLogin}
-                                    >
-                                        Sign In
-                                    </Button>
-                                    <Grid container>
-                                        <Grid item xs>
-                                            <Link to="/signup" style={{ textDecoration: 'none' }}>
-                                                <Button variant="outlined" color="secondary" sx={{
-                                                    boxShadow: 'none', // Disable the default box-shadow
-                                                    fontSize: '10px', // Larger font size
-                                                    textTransform: 'none', // Prevent uppercase transform
-                                                }}>
-                                                    Forgot Password?
-                                                </Button>
-                                            </Link>
-                                        </Grid>
-                                        <Grid item xs>
-                                            <Link to="/signup" style={{ textDecoration: 'none' }}>
-                                                <Button variant="outlined" color="secondary" sx={{
-                                                    boxShadow: 'none', // Disable the default box-shadow
-                                                    fontSize: '10px', // Larger font size
-                                                    textTransform: 'none', // Prevent uppercase transform
-                                                }}>
-                                                    Don't have an account?
-                                                </Button>
-                                            </Link>
-                                        </Grid>
-                                    </Grid>
-                                </Box>
-                            </Box>
-                        </Grid>
-                    </Grid>
-                </Box>
-            </Container>
+            {isMobile ? (<Container component="main" maxWidth={"xl"}>
+                {loginBox}
+            </Container>) : (loginBox)}
         </>
     );
 }
--- frontend/ez-monetization/src/pages/signup/signup.tsx
+++ frontend/ez-monetization/src/pages/signup/signup.tsx
@@ -3,34 +3,36 @@ import {
     Avatar,
     Box,
     Button,
+    Checkbox,
     Container,
     CssBaseline,
+    FormControlLabel,
     Grid,
+    Paper,
     TextField,
     ThemeProvider,
     Typography,
+    useMediaQuery,
 } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import { useNavigate, Link } from "react-router-dom";
 import { LockOutlined } from "@mui/icons-material";
 import { signUp } from "../../services/auth.service";
 import { ISignUp } from "../../models/auth.model";
+import theme from "../../theme/ThemeProvider";
+import { toast } from "react-toastify";
+import { useSignal } from "@preact/signals-react";
 
 export default function Signup(): JSX.Element {
     const [inputState, setInputState] = useState<ISignUp>({
         username: "",
         password: "",
-        confirm_password: "",
         first_name: "",
         last_name: "",
     });
 
-    const [isDisabled, setIsDisabled] = useState<boolean>(true);
-
-    const { username, password, confirm_password, first_name, last_name } =
-        inputState;
+    const checkedSignal = useSignal(false);
     const navigate = useNavigate();
-    const defaultTheme = createTheme();
 
     function handleInput(event: React.ChangeEvent<HTMLInputElement>) {
         setInputState({
@@ -39,32 +41,19 @@ export default function Signup(): JSX.Element {
         });
     }
 
-    useEffect(() => {
-        if (
-            username === "" ||
-            password === "" ||
-            confirm_password === "" ||
-            first_name === "" ||
-            last_name === ""
-        ) {
-            setIsDisabled(true);
-        }
-        if (password === confirm_password) {
-            setIsDisabled(false);
-        } else {
-            setIsDisabled(true);
-        }
-    }, [
-        password,
-        confirm_password,
-        username,
-        first_name,
-        last_name,
-        handleInput,
-    ]);
 
     function handleRegistration(): void {
         const { username, password, first_name, last_name } = inputState;
+        if (username === "" || password === "" || first_name === "" || last_name === "") {
+            toast.error("Please fill in all fields");
+            return;
+        }
+
+        if (!checkedSignal.value) {
+            toast.error("Please accept the Terms and Conditions");
+            return;
+        }
+
         let payload = {
             username: username,
             password: password,
@@ -72,64 +61,110 @@ export default function Signup(): JSX.Element {
             first_name: first_name,
             last_name: last_name,
         };
+
         signUp(payload);
-        console.log(payload);
     }
 
-    return (
-        <ThemeProvider theme={defaultTheme}>
-            <Container component="main" maxWidth="xs">
-                <CssBaseline />
+    const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
+    const signUpBox = <Box
+        sx={{
+            marginTop: 4,
+            marginBottom: 4,
+        }}
+    >
+        <Grid container>
+            <CssBaseline />
+            <Grid
+                item
+                xs={false}
+                sm={4}
+                md={6}
+                sx={{
+                    backgroundImage: "url(https://source.unsplash.com/random)",
+                    backgroundRepeat: "no-repeat",
+                    backgroundColor: (t) =>
+                        t.palette.mode === "light"
+                            ? t.palette.grey[50]
+                            : t.palette.grey[900],
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                    borderRadius: "16px 0 0 16px",
+                }}
+            />
+            <Grid
+                item
+                xs={12}
+                sm={8}
+                md={6}
+                p={isMobile ? -2 : 4}
+                component={Paper}
+                elevation={0}
+                square
+                sx={{
+                    borderTopRightRadius: '16px', // Set the top-right border-radius
+                    borderBottomRightRadius: '16px',
+                    borderBottomLeftRadius: isMobile ? '16px' : '0px',
+                    borderTopLeftRadius: isMobile ? '16px' : '0px', // Set the bottom-right border-radius
+                }}
+            >
                 <Box
                     sx={{
-                        marginTop: 8,
+                        my: 8,
+                        mx: 4,
                         display: "flex",
                         flexDirection: "column",
                         alignItems: "center",
                     }}
                 >
-                    <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-                        <LockOutlined />
-                    </Avatar>
-                    <Typography component="h1" variant="h5">
-                        Sign up
+
+                    <Typography component="h1" variant="h3">
+                        Sign Up
+                    </Typography>
+                    <Typography component={"p"} fontWeight={"light"}>
+                        Fill in the fields below to sign up for an account.
                     </Typography>
                     <Box
                         component="form"
                         noValidate
-                        onSubmit={handleRegistration}
+                        onSubmit={(e) => { e.preventDefault(); }}
                         sx={{ mt: 3 }}
                     >
                         <Grid container spacing={2}>
                             <Grid item xs={12} sm={6}>
                                 <TextField
                                     autoComplete="given-name"
-                                    name="firstName"
+                                    name="first_name"
                                     required
                                     fullWidth
-                                    id="firstName"
+                                    id="first_name"
                                     label="First Name"
                                     autoFocus
+                                    onChange={handleInput}
                                 />
                             </Grid>
                             <Grid item xs={12} sm={6}>
                                 <TextField
                                     required
                                     fullWidth
-                                    id="lastName"
+                                    id="last_name"
                                     label="Last Name"
-                                    name="lastName"
+                                    name="last_name"
                                     autoComplete="family-name"
+                                    onChange={handleInput}
+
                                 />
                             </Grid>
                             <Grid item xs={12}>
                                 <TextField
                                     required
                                     fullWidth
-                                    id="email"
+                                    id="username"
                                     label="Email Address"
-                                    name="email"
+                                    name="username"
                                     autoComplete="email"
+                                    onChange={handleInput}
+
                                 />
                             </Grid>
                             <Grid item xs={12}>
@@ -141,26 +176,48 @@ export default function Signup(): JSX.Element {
                                     type="password"
                                     id="password"
                                     autoComplete="new-password"
+                                    onChange={handleInput}
+
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <FormControlLabel
+                                    control={<Checkbox value="remember" color="primary" onChange={(e) => { checkedSignal.value = e.target.checked }} />}
+                                    label="I accept the Terms and Conditions"
+
                                 />
                             </Grid>
                         </Grid>
                         <Button
-                            type="submit"
                             fullWidth
                             variant="contained"
                             sx={{ mt: 3, mb: 2 }}
-                            disabled={isDisabled}
+                            onClick={handleRegistration}
                         >
                             Sign Up
                         </Button>
-                        <Grid container justifyContent="flex-end">
-                            <Grid item>
-                                <Link to="/login">Already have an account? Sign in</Link>
-                            </Grid>
+                        <Grid item xs={12} md={12} mt={2} >
+                            <Link to="/login" style={{ textDecoration: 'none' }}>
+                                <Button variant="outlined" color="secondary" sx={{
+                                    boxShadow: 'none', // Disable the default box-shadow
+                                    fontSize: '10px', // Larger font size
+                                    textTransform: 'none', // Prevent uppercase transform
+                                }}>
+                                    Already have an account? Sign In.
+                                </Button>
+                            </Link>
                         </Grid>
                     </Box>
                 </Box>
-            </Container>
-        </ThemeProvider>
+            </Grid>
+        </Grid>
+    </Box>
+
+    return (
+        <>
+            {isMobile ? (<Container component="main" maxWidth={"xl"}>
+                {signUpBox}
+            </Container>) : (signUpBox)}
+        </>
     );
 }
